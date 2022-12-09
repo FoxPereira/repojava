ce6bc7f9811eea25e738135904f889ffa5f0f8e7
FoxPereira <felipepereiraenf@gmail.com>
2022-12-09 00:10:16 -0300

    Update main.js
    Tarefa da semana 2 de java

--- src/main.js
+++ src/main.js
@@ -1,6 +1,114 @@
-var resultado;
+// Array de 10 elementos
+var elementos = ['primeiro', 'segundo', 'terceiro', 'quarto', 'quinto', 'sexto', 'sétimo', 'oitavo', 'nono', 'último'];
+
+console.log (elementos.length);
+// 10
+
+ var primeiro = elementos [0];
+ // primeiro
+
+ var ultimo = elementos[9];
+ // último
+
+ elementos.forEach(function (item, indice, array) {
+    console.log(item, indice);
+  });
+  
+
+  var elementos = ['primeiro', 'segundo', 'terceiro', 'quarto', 'quinto', 'sexto', 'sétimo', 'oitavo', 'nono', 'último'];
+
+  while (true)
+ {
+    console.log(elementos);
+    break;
+
+ }
+
+ var elementos = ['primeiro', 'segundo', 'terceiro', 'quarto', 'quinto', 'sexto', 'sétimo', 'oitavo', 'nono', 'último'];
+
+ do
+ {
+    console.log(elementos);
+    break;
+ }
+ while(true)
 
 for(let conta =1; conta <= 10; conta++){
     resultado = conta * 2 ;
-    console.log('Tabuada do 2 = ' + conta + ' *2 = ' + resultado)
+    console.log('Tabuada do 2 >>> ' + conta + ' x 2 = ' + resultado)
+}
+
+// Array de 5 elementos
+// var numeros = ['4', '9', '16', '25', '36'];
+
+
+{
+    var numero = ['a', 'b', 'c', 'd', 'e'];
+    a = 4;
+    b = 9;
+    c = 16;
+    d = 25;
+    e = 36;
+
+    var resultado = a * a;
+    {console.log('o quadrado de ' + a + ' é ' + resultado)}
+    var resultado = b * b;
+    {console.log('o quadrado de ' + b + ' é ' + resultado)}
+    var resultado = c * c;
+    {console.log('o quadrado de ' + c + ' é ' + resultado)}
+    var resultado = d * d;
+    {console.log('o quadrado de ' + d + ' é ' + resultado)}
+    var resultado = e * e;
+    {console.log('o quadrado de ' + e + ' é ' + resultado)}
+    
+    var resultado = Math.sqrt(a);
+    {console.log('a raiz quadrada de ' + a + ' é ' + resultado)}
+    var resultado = Math.sqrt(b);
+    {console.log('a raiz quadrada de ' + b + ' é ' + resultado)}
+    var resultado = Math.sqrt(c);
+    {console.log('a raiz quadrada de ' + c + ' é ' + resultado)}
+    var resultado = Math.sqrt(d);
+    {console.log('a raiz quadrada de ' + d + ' é ' + resultado)}
+    var resultado = Math.sqrt(e);
+    {console.log('a raiz quadrada de ' + e + ' é ' + resultado)}
+
+    var resultado = Math.abs (a);
+    {console.log('a parte inteira de  ' + a + ' é ' + resultado)}
+    var resultado = Math.abs (b);
+    {console.log('a parte inteira de  ' + b + ' é ' + resultado)}
+    var resultado = Math.abs (c);
+    {console.log('a parte inteira de  ' + c + ' é ' + resultado)}
+    var resultado = Math.abs (d);
+    {console.log('a parte inteira de  ' + d + ' é ' + resultado)}
+    var resultado = Math.abs (e);
+    {console.log('a parte inteira de  ' + e + ' é ' + resultado)}
+
+    var resultado = Math.ceil (a);
+    {console.log( a +' arredondado para cima fica  ' + resultado)}
+    var resultado = Math.ceil (b);
+    {console.log( b +' arredondado para cima fica  ' + resultado)}
+    var resultado = Math.ceil (c);
+    {console.log( c +' arredondado para cima fica  ' + resultado)}
+    var resultado = Math.ceil (d);
+    {console.log( d +' arredondado para cima fica  ' + resultado)}
+    var resultado = Math.ceil (e);
+    {console.log( e +' arredondado para cima fica  ' + resultado)}
+
+    var resultado = Math.floor (a);
+    {console.log( a +' arredondado para baixo fica  ' + resultado)}
+    var resultado = Math.floor (b);
+    {console.log( b +' arredondado para baixo fica  ' + resultado)}
+    var resultado = Math.floor (c);
+    {console.log( c +' arredondado para baixo fica  ' + resultado)}
+    var resultado = Math.floor (d);
+    {console.log( d +' arredondado para baixo fica  ' + resultado)}
+    var resultado = Math.floor (e);
+    {console.log( e +' arredondado para baixo fica  ' + resultado)}
+
+    // for let numeros= 0; conta= numeros 
+    // {
+    //     resultado = numeros * numeros ;
+    //     console.log('o quadrado de' + numeros + 'é' + resultado)
+    // }
+ 
 }
